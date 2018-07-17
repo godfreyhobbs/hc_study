5d8b4c51b9bd062710881af14b89ff498c649ebd
godfreyhobbs <godfreyhobbs@gmail.com>
2018-07-17 16:15:44 -0600

    snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+
+it('renders correctly', () => {
+  const tree = renderer
+.create(<App page="http://www.facebook.com">Facebook</App>)
+   .toJSON();
+expect(tree).toMatchSnapshot();
 });
