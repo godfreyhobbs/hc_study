3e8bdb121a33a452a808027eab1da28585d2b205
anthony.albertorio <anthony.albertorio@gmail.com>
2018-07-18 11:47:02 -0600

    added IframeQuestions component to Components folder.

---
+++ src/Components/IframeQuestions.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class IframeQuestions extends Component {
+
+  constructor(props) {
+    super(props);
+    this.questionForm = 'https://docs.google.com/forms/d/1UVUlJlM1hy1wn2_54fuweYm4tkfvUtYycAF8FB8rhxM/viewform?edit_requested=true';
+  }
+
+  render() {
+    return (
+       <div>
+         <iframe src={this.questionForm} height="3550px" width="70%"/>
+       </div>
+     );
+  }
+}
+
+export default IframeQuestions;
