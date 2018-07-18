04e3543a0ef0f02efdddd4f4dccefb585efecbb2
anthony.albertorio <anthony.albertorio@gmail.com>
2018-07-17 22:15:39 -0600

    fixed issue, returning with single div now.

--- src/Components/ScreeningResults.js
+++ src/Components/ScreeningResults.js
@@ -4,9 +4,11 @@ import logo from '../logo.svg';
 class ScreeningResults extends Component {
   render() {
     return (
+      <div>
         <h1 className="App-title">Screening Results</h1>
         <p>Congratulations! You are eligible for this study.</p>
         <button>Start Questionnaire</button>
+      </div>
     );
   }
 }
