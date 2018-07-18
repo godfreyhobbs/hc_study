384916b09f6cbd0009590ae57e34827efcc00a79
godfreyhobbs <godfreyhobbs@gmail.com>
2018-07-18 10:53:10 -0600

    passing props and formatting

--- src/Components/GrantAccessForScreening.js
+++ src/Components/GrantAccessForScreening.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../App.css';
 
 class GrantAccessForScreening extends Component {
 
   handleClick() {
+    // this.props.linniaPermissionsInstance;
+    // this.props.web3
+    // this.props.accounts
+
+
     this.props.callback("ScreeningResults");
   }
 
   render() {
     return (
-      <div>
-        <div className="terms-and-conditions">
-          <p>Some information to grant access to Viel Cornwell to use your data.</p>
-          <p>Something about you understand how we will use data, when and where</p>
-          <p>We won't hold your data or something, but look at it to see if you are eligble, etc.</p>
-        </div>
+       <div>
+         <div className="terms-and-conditions">
+           <p>Some information to grant access to Viel Cornwell to use your data.</p>
+           <p>Something about you understand how we will use data, when and where</p>
+           <p>We won't hold your data or something, but look at it to see if you are eligble, etc.</p>
+         </div>
 
-        <div className="medical-summary">
-          <div className="FHIR-docs">FHIR Docs</div>
-          <div className="medical-overview">Medical Record Summary</div>
-          <div className="hospital">Joe's Hospital</div>
-        </div>
+         <div className="medical-summary">
+           <div className="FHIR-docs">FHIR Docs</div>
+           <div className="medical-overview">Medical Record Summary</div>
+           <div className="hospital">Joe's Hospital</div>
+         </div>
 
-        <p>Hey, man I understand and agree to share my Medical Record info with Viel Cornwell</p>
-        <button onClick={(e) => this.handleClick(e)}>
-          I Agree</button>
-
-      </div>
+         <p>Hey, man I understand and agree to share my Medical Record info with Viel Cornwell</p>
+         <button onClick={(e) => this.handleClick(e)}>
+           I Agree
+         </button>
+       </div>
     );
   }
 }
