fbfc2b2a42a5b9e45885e97178830214160c0d53
godfreyhobbs <godfreyhobbs@gmail.com>
2018-08-20 18:29:11 -0700

    lint

--- src/Components/Consent.js
+++ src/Components/Consent.js
@@ -4,7 +4,7 @@ import React from "react";
 function Consent(props) {
   return (<div>
     <button onClick={props.consent}>Digitally Sign Consent</button><h1>
-    <a target="_blank" href="./consent.txt">Print or download the consent form for your records</a></h1>
+      <a target='_blank' href='./consent.txt'>Print or download the consent form for your records</a></h1>
   </div>);
 }
 
